3c11ba0f6029e839a8e75bbffd53cb03735c37fe
jing Wang <admin@jingwangnet.com>
2021-10-03 12:58:02 +0800

    Use unittest in functional_tests.py.

--- functional_tests.py
+++ functional_tests.py
@@ -1,21 +1,29 @@
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
-
-options = Options()
-options.headless = True
+import unittest
 
 
-try:
-    browser = webdriver.Firefox(options=options)
-    browser.get('http://127.0.0.1:8000')
-except WebDriverException:
-    pass   
+class NewVisitorTest(unittest.TestCase):
+   
 
-try:
-    assert 'success' in browser.title, browser.title
-except AssertionError as e:
-    print(e)
-finally:
-    browser.close()
+   def setUp(self):
+       options = Options()
+       options.headless = True
 
+       self.browser = webdriver.Firefox(options=options)
+
+   def tearDown(self):
+       self.browser.close()
+
+   def test_start_a_list_and_retrieve_it_later(self):
+       try:
+           self.browser.get('http://127.0.0.1:8000')
+       except WebDriverException:
+           pass   
+       
+       self.assertIn('succese', self.browser.title)
+       
+
+if __name__ == '__main__':
+    unittest.main()
