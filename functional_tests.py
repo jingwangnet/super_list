aae2754419623a5490cda603b509c1aced0501ed
jing Wang <admin@jingwangnet.com>
2021-10-03 14:35:02 +0800

    Add user story for functional test.

--- functional_tests.py
+++ functional_tests.py
@@ -17,13 +17,27 @@ class NewVisitorTest(unittest.TestCase):
        self.browser.close()
 
    def test_start_a_list_and_retrieve_it_later(self):
+       # John visits the website
        try:
            self.browser.get('http://127.0.0.1:8000')
        except WebDriverException:
            pass   
        
+       # There is "To-Do" in the title and header_text
        self.assertIn('To-Do', self.browser.title)
+
+       # There is a inputbox
+
+       # 'Submit a item for saving' in the inputbox
+
+       # John submit 'I will go shoping.'
        
+       # '1. I will go shoping.' appears the table of the page.
+
+       # John submit 'I will have a date with mary.' agian
+
+       # '2. I will have a date with mary.' appears the table of the page.
+
 
 if __name__ == '__main__':
     unittest.main()
