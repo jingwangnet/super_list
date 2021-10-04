9f0f48dacbf0573423ffb532dc76037b2f30e1f6
jing Wang <admin@jingwangnet.com>
2021-10-04 10:06:16 +0800

    Updaet user story for functional test.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,18 +90,39 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_create_multiple_list_at_the_diffrent_url(self):
         # John visits the website 
+        self.browser.get(self.live_server_url)
         # John submits 'I will have a date with mary'
+        inputbox = self.browser.find_element(By.ID, 'id-new-item')
+        inputbox.send_keys('I will have a date with mary')
+        inputbox.send_keys(Keys.ENTER)
         # He saw '1. I will have a date with mary' after the page redirected
-        # He feels good and leaving
+        self.wait_for_check_row_in_the_table('1. ' + 'I will have a date with mary')
         # the url match the URL pattern '/list/.*/'
+        URL_OF_JOHN = self.browser.current_url
+        self.assertRegex(URL_OF_JOHN, '/list/.*/')
+        # He feels good and leaving
+        self.browser.quit()
 
         # Joe visits the website too
+        self.setUp()
+        self.browser.get(self.live_server_url)
         # Joe expects that item of John does not appear the page 
+        html = self.browser.page_source
+        self.assertNotIn('I will have a date with mary', html)
         # Joe submits 'Do my homework'  
+        inputbox = self.browser.find_element(By.ID, 'id-new-item')
+        inputbox.send_keys('Do my home work')
+        inputbox.send_keys(Keys.ENTER)
         # He saw '1. Do my homework'
+        self.wait_for_check_row_in_the_table('1. ' + 'Do my homework')
         # Joe expects that item of John does not appear the page 
+        html = self.browser.page_source
+        self.assertNotIn('I will have a date with mary', html)
         # the url match the URL pattern '/list/.*/'
+        URL_OF_JOE = self.brower.current_url
+        self.assertRegex(URL_OF_JOE, '/list/.*/')
         # the url is not same as John's 
+        self.asserrtNotEqual(URL_OF_JOE, URL_OF_JOHN)
         
         
 
