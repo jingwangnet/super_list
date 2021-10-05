8bc77b7bc8c7179ea2c17f18d26ff235f71b29f9
jing Wang <admin@jingwangnet.com>
2021-10-05 11:06:05 +0800

    FT styling and layout first step.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -124,6 +124,31 @@ class NewVisitorTest(LiveServerTestCase):
         # the url is not same as John's 
         self.assertNotEqual(URL_OF_JOE, URL_OF_JOHN)
         
-        
+
+    def test_styling_and_layout(self):
+        # John visit the website
+        self.browser.set_window_size(1024, 768)
+        self.browser.get(self.live_server_url)
+        # John want to detect the input of the home_page is center or not.
+        inputbox = self.browser.find_element(By.ID, 'id-new-item')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # John submit some item 
+        inputbox.send_keys('I will have a date with mary')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_check_row_in_the_table('1. ' + 'I will have a date with mary')
+
+        # John want to detecot the inputbox of the view_list  is center or not
+        inputbox = self.browser.find_element(By.ID, 'id-new-item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 
